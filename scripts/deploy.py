0ecc4f8516826b3c247e8d2d725589008c950096
Dani Bachs <danibachsl@protonmail.com>
2022-10-24 11:57:07 +0200

    etherscan API added

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,6 +4,8 @@ from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
+    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    print("Contract deployed to {fund_me.address}")
 
 
 def main():
