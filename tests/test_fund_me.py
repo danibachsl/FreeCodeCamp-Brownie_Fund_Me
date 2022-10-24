6ec0d69f55c3f1391fd15c554c0882320f43af9f
Dani Bachs <danibachsl@protonmail.com>
2022-10-24 16:58:02 +0200

    more changes

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,6 +1,6 @@
 from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 from scripts.deploy import deploy_fund_me
-from brownie import network, accounts
+from brownie import network, accounts, exceptions
 import pytest
 
 
@@ -19,7 +19,7 @@ def test_can_fund_and_withdraw():
 def test_only_owner_can_withdraw():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("only for local testing")
-    account = get_account()
     fund_me = deploy_fund_me()
     bad_actor = accounts.add()
-    fund_me.withdraw({"from": bad_actor})
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
